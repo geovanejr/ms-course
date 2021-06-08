2598c19a06eb03f0e21d5b95274bc0b114f1dd98
Geovane Junior <geovane.gjunior@gmail.com>
2021-06-08 10:23:29 -0300

    Feign Client

--- hr-worker/src/main/resources/data.sql
+++ hr-worker/src/main/resources/data.sql
@@ -2,3 +2,4 @@ INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 200.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 300.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 250.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Geovane', 400.00);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Denise', 150.00);
