4eb694ebf0ffb6b289011c73138d8b27797c5272
Geovane Junior <geovane.gjunior@gmail.com>
2021-06-09 18:05:28 -0300

    Database seeding

---
+++ hr-user/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Nina Brown', 'nina@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+INSERT INTO tb_user (name, email, password) VALUES ('Geovane Junior', 'geovane.gjunior@gmail.com', '$2a$10$ApdrLLOcc9SaH6dskUr6yeh.PJpwZxgn4HrMI3HXObiJCt/MRH.PW');
+
+INSERT INTO tb_role (role_name) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (role_name) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
