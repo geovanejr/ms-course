850ff43dba935de9b86f4ff1bd5fb09fcac89bff
Geovane Junior <geovane.gjunior@gmail.com>
2021-06-10 20:03:43 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/br/com/geovanejunior/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geovanejunior.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
